880dc74d184e65c1a91ca5cc7dc5b9f822b73d8a
Your Name <you@example.com>
2019-03-16 14:32:32 +0600

    expense

--- app/src/main/java/com/sohojhisab/BalanceStatement.java
+++ app/src/main/java/com/sohojhisab/BalanceStatement.java
@@ -78,6 +78,8 @@ public class BalanceStatement extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.balance_statement);
 
+        Toast.makeText(this, "Balance", Toast.LENGTH_SHORT).show();
+
         db = new DatabaseHandler(this);
         db1 = new DatabaseHandler1(this);
         dbUser = new DatabaseHandlerUser(this);
