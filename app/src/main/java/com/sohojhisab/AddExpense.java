28777ed1c007969bb869b1df9bc0581d0ed6b54a
Your Name <you@example.com>
2019-03-16 14:23:29 +0600

    expense

--- app/src/main/java/com/sohojhisab/AddExpense.java
+++ app/src/main/java/com/sohojhisab/AddExpense.java
@@ -1,5 +1,6 @@
 package com.sohojhisab;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
@@ -9,6 +10,7 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -18,7 +20,10 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.view.menu.MenuPopupHelper;
@@ -90,6 +95,8 @@ public class AddExpense extends AppCompatActivity {
         uname = result1[1];
         //Toast.makeText(this, pass, Toast.LENGTH_SHORT).show();
         password = result1[2];
+
+        verifyPermissions();
 
         fontAwesomeFont = Typeface.createFromAsset(getAssets(), "fa-solid-900.ttf");
 
@@ -217,8 +224,10 @@ public class AddExpense extends AppCompatActivity {
         take.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(photoCaptureIntent, requestCode);
+                /*Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(photoCaptureIntent, requestCode);*/
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent, requestCode);
             }
         });
 
@@ -381,6 +390,28 @@ public class AddExpense extends AppCompatActivity {
         });
 
         ShowRecords();
+    }
+
+    private void verifyPermissions(){
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[1]) == PackageManager.PERMISSION_GRANTED
+                /*&& ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[2]) == PackageManager.PERMISSION_GRANTED*/){
+            //Toast.makeText(CameraShopping.this, "ইতিমধ্যে পার্মিশনগুলো নেওয়া হয়েছে!", Toast.LENGTH_SHORT).show();
+        }else{
+            ActivityCompat.requestPermissions(AddExpense.this, permissions, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        verifyPermissions();
     }
 
     @Override
